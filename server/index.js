87ac98cef1b4faa4f7c456d6cfa99d4fde38417c
Yordanos Yeshitla <yordiman614@gmail.com>
2024-10-28 21:01:02 +0300

    some changes on the file structure of the react app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected To database."))
+.catch((error) => console.error("Falid to Connect." + error))
 
 
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is started on http://localhost:${process.env.PORT}.`)
+})
