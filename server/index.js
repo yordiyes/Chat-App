4743bda563d3ec35d5bf2bad74e2c91b806f9a29
Yordanos Yeshitla <yordiman614@gmail.com>
2024-10-30 16:50:25 +0300

    i have been working on the register functionality for user in order to register to the app

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", userRoutes);
 
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
