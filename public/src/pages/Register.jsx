b96a87f40da416caa89214f82ac56d081db1d8ef
Yordanos Yeshitla <yordiman614@gmail.com>
2024-11-01 11:35:29 +0300

    I have been working on the login functionality of the website and add this functionality to it

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 import Logo from "../assets/logo.svg";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+import { registerRoute } from '../utils/APIRoutes';
 
 function Register() {
+  const navigate = useNavigate();
   const [values, setValues] = useState({
     username: "",
     email: "",
@@ -13,22 +16,49 @@ function Register() {
     confirmPassword: ""
   });
 
-  const handleSubmit = (event) => {
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 5000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark"
+  };
+
+  const handleSubmit = async(event) => {
     event.preventDefault();
-    handleValidation();
+    if (handleValidation()) {
+      const { username, email, password} = values;
+      const {data} = await axios.post(registerRoute,{
+        username,
+        email,
+        password
+      });
+      if(data.status===false){
+        toast.error(data.msg, toastOptions)
+      }
+      if(data.status===true){
+        localStorage.setItem('chat-app-user', JSON.stringify(data.user));
+      }
+      navigate("/");
+    }
   };
 
   const handleValidation = () => {
-    const { password, confirmPassword } = values;
+    const { username, email, password, confirmPassword } = values;
     if (password !== confirmPassword) {
-      toast.error("Password and confirm password should be the same.", {
-        position: "bottom-right",  // Updated to match ToastContainer position
-        autoClose: 8000,
-        pauseOnHover: true,
-        draggable: true,
-        theme: "dark"
-      });
+      toast.error("Password and confirm password should be the same.", toastOptions);
+      return false;
+    } else if (username.length < 3) {
+      toast.error("Username should be greater than 3 characters.", toastOptions);
+      return false;
+    } else if (password.length < 8) {
+      toast.error("Password should be equal or greater than 8 characters.", toastOptions);
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
     }
+    return true;
   };
 
   const handleChange = (event) => {
@@ -73,7 +103,7 @@ function Register() {
           </span>
         </form>
       </FormContainer>
-      <ToastContainer position="top-right" autoClose={5000} />
+      <ToastContainer />
     </>
   );
 }
