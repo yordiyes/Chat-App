0ef0620b3a9e37f53fcffaf2f99cdb4703673b71
Yordanos Yeshitla <yordiman614@gmail.com>
2024-11-28 16:05:32 +0300

    Added a Welcome components

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 import { allUsersRoute } from "../utils/APIRoutes";
 import Contacts from '../components/Contacts';
+import Welcome from '../components/Welcome';
 
 function Chat() {
   const navigate = useNavigate();
@@ -50,7 +51,12 @@ function Chat() {
   };
   return <Container>
       <div className="container">
-        <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange}/>
+        <Contacts 
+          contacts={contacts} 
+          currentUser={currentUser} 
+          changeChat={handleChatChange}
+        />
+        <Welcome currentUser={currentUser}/>
       </div>
   </Container>
   
