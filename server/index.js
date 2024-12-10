534e1b645a8c0d13d7f8f4f27716990b72b1a843
Yordanos Yeshitla <133559301+yordiyes@users.noreply.github.com>
2024-12-10 20:14:16 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
+const messageRoute = require("./routes/messagesRoute");
+
 
 const app = express();
 
@@ -10,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoute);
+
 
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
